291431e35fb1b9c2e925a17ed1ed9ac16bbfe747
ViktorKryvorotov <113522935+ViktorKryvorotov@users.noreply.github.com>
2023-02-21 14:27:18 +0200

    markup

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,41 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+
+  console.log(galleryItems);
+
+const setGallery = document.querySelector('.gallery');
+const galleryMarkup = creatGalleryElMarkup(galleryItems);
+setGallery.insertAdjacentHTML('beforeend', galleryMarkup);
+
+function creatGalleryElMarkup(galleryItems) {
+    return galleryItems.map(({ preview, original, description }) => 
+       `<div class="gallery__item">
+        <a class="gallery__link" href="${original}">
+        <img
+        class="gallery__image"
+        src="${preview}"
+        data-source="${original}"
+        alt="${description}"
+        />
+        </a>
+        </div>`
+    ).join('');
+  
+}
+
+
+// import * as basicLightbox from 'basiclightbox'
+
+// const instance = basicLightbox.create(`
+//     <img src="assets/images/image.png" width="800" height="600">
+// `)
+
+// instance.show()
+
+
+
+
+
+
+
--- js/gallery-items.js
+++ js/gallery-items.js
@@ -1,7 +1,7 @@
 export const galleryItems = [
   {
     preview:
-      'https://pixabay.com/get/g7db44fc58d9d35fb5b8da7664b7bad4f2a50cea6b119384ab39a8a881687d67f82dc1a7e35dc15af734160ce74143ae63fdbc1684fe099b2779e8cbab6064ab0_640.jpg',
+      'https://pixabay.com/get/g70eec56ac96b5fcacddc04d4aad6cb933b44838e5decf1dc82cc123b7363336317a98c50c3f38314f3a8b3cdc7330a10506e5611072bdbaec93e7d920b528fb6066e9f14cf0332d63f7b35346fa56bd3_640.jpg',
     original:
       'https://pixabay.com/get/gf802aba3d6b7ef3f231b37447013a653b19fbb26a1f3c79a12594483d040f9c884704948c0f1a4f8ac1a53e4b682c8b44d4be8692329d42bf8a9dbe5d84368fb_1280.jpg',
     description: 'Hokkaido Flower',
